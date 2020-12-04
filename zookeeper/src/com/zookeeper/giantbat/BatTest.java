229875c7cd08dcf58fcd0a4ba0ade54c3f4f7d5b
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-03 21:29:20 -0800

    Created BatTest class to preform methods

---
+++ zookeeper/src/com/zookeeper/giantbat/BatTest.java
@@ -0,0 +1,20 @@
+package com.zookeeper.giantbat;
+
+public class BatTest extends Bat {
+
+	public static void main(String[] args) {
+		Bat vamp = new Bat();
+		vamp.displayEnergy();
+		vamp.attackTown();
+		vamp.attackTown();
+		vamp.attackTown();
+		vamp.displayEnergy();
+		vamp.eatHumans();
+		vamp.eatHumans();
+		vamp.displayEnergy();
+		vamp.fly();
+		vamp.fly();
+		vamp.displayEnergy();
+	}
+
+}
