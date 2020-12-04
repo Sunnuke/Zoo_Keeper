c4c7ad1a54038f685d7c28ff1d200fc9dd7f4307
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-03 19:40:08 -0800

    Created Mammal class

---
+++ zookeeper/src/com/zookeeper/gorilla/Mammal.java
@@ -0,0 +1,11 @@
+package com.zookeeper.gorilla;
+
+public class Mammal {
+//	Member Variables
+	private int energyLevel;
+	
+//	Member Methods
+	public int displayEnergy() {
+		return this.energyLevel;
+	}
+}
